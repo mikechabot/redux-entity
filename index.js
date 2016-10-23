ba101de9df4eaddb08a36f23ea47a0472c98213f
Mike Chabot <mike.chabot@live.com>
2016-10-23 02:40:35 -0400

    Adding resetEntity and deletEntity to exports

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict';
 
-const reducer = require('./src/reducer')
+const reducer = require('./src/reducer');
 const loadEntity = require('./src/load-entity-thunk');
+const { resetEntity, deleteEntity } = require('./src/action-creators');
 
 module.exports = {
     reducer: reducer,
-    loadEntity: loadEntity
+    loadEntity: loadEntity,
+    resetEntity: resetEntity,
+    deleteEntity: deleteEntity
 };
