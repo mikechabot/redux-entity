a43134781a73e8c3f1a6b81dda8d36e78a21338a
Mike Chabot <mike.chabot@live.com>
2019-02-03 02:17:01 -0500

    Update preprocessors for success/failure. Update tests.

--- src/common/entity-lifecycle.js
+++ src/common/entity-lifecycle.js
@@ -1,21 +1,21 @@
 import EntityConfiguration from './entity-configuration';
 import actionCreators from '../action-creators';
-import { PROCESSOR_STAGE } from './entity-const';
+import {PROCESSOR_STAGE} from './entity-const';
 
 function EntityLifecycle(entityName, options) {
-  if (!entityName) throw new Error('Missing required entity name');
-  this.entityName = entityName;
-  this.config = new EntityConfiguration(options || {});
-  this.dispatch = null;
+    if (!entityName) throw new Error('Missing required entity name');
+    this.entityName = entityName;
+    this.config = new EntityConfiguration(options || {});
+    this.dispatch = null;
 }
 
 EntityLifecycle.prototype.getEntityName = function () {
-  return this.entityName;
+    return this.entityName;
 };
 
 EntityLifecycle.prototype.setDispatch = function (dispatch) {
-  if (typeof dispatch !== 'function') throw new Error('dispatch must be a function');
-  this.dispatch = dispatch;
+    if (typeof dispatch !== 'function') throw new Error('dispatch must be a function');
+    this.dispatch = dispatch;
 };
 
 EntityLifecycle.prototype.setGetState = function (getState) {
@@ -24,63 +24,64 @@ EntityLifecycle.prototype.setGetState = function (getState) {
 };
 
 EntityLifecycle.prototype.getDispatch = function () {
-  if (!this.dispatch) throw new Error('Missing required dispatch function');
-  return this.dispatch;
+    if (!this.dispatch) throw new Error('Missing required dispatch function');
+    return this.dispatch;
 };
 
 EntityLifecycle.prototype.onLoad = function () {
-  if (!this.config.isSilent()) {
-    this.getDispatch()(actionCreators.fetchRequest(this.getEntityName())());
-  }
+    if (!this.config.isSilent()) {
+        this.getDispatch()(actionCreators.fetchRequest(this.getEntityName())());
+    }
 };
 
 EntityLifecycle.prototype.onSuccess = function (data) {
-  this.runBeforeSuccess(data);
-  this.dispatchFetchSuccess(data);
-  this.runAfterSuccess(data);
+    const dispatchedData = this.runBeforeSuccess(data);
+    this.dispatchFetchSuccess(dispatchedData);
+    this.runAfterSuccess(dispatchedData);
 };
 
 EntityLifecycle.prototype.onFailure = function (error) {
-  this.runBeforeFailure(error);
-  this.dispatchFetchFailure(error);
-  this.runAfterFailure(error);
+    const dispatchedError = this.runBeforeFailure(error);
+    this.dispatchFetchFailure(dispatchedError);
+    this.runAfterFailure(dispatchedError);
 };
 
 EntityLifecycle.prototype.runBeforeSuccess = function (data) {
-  this.processStage(PROCESSOR_STAGE.BEFORE_SUCCESS, data);
+    return this.processStage(PROCESSOR_STAGE.BEFORE_SUCCESS, data);
 };
 
 EntityLifecycle.prototype.runAfterSuccess = function (data) {
-  this.processStage(PROCESSOR_STAGE.AFTER_SUCCESS, data);
+    return this.processStage(PROCESSOR_STAGE.AFTER_SUCCESS, data);
 };
 
 EntityLifecycle.prototype.runBeforeFailure = function (error) {
-  this.processStage(PROCESSOR_STAGE.BEFORE_FAILURE, error);
+    return this.processStage(PROCESSOR_STAGE.BEFORE_FAILURE, error);
 };
 
 EntityLifecycle.prototype.runAfterFailure = function (error) {
-  this.processStage(PROCESSOR_STAGE.AFTER_FAILURE, error);
+    return this.processStage(PROCESSOR_STAGE.AFTER_FAILURE, error);
 };
 
 EntityLifecycle.prototype.dispatchFetchSuccess = function (data) {
-  this.dispatch(
-    actionCreators.fetchSuccess(this.getEntityName())(data, Date.now(), this.config.doAppend())
-  );
+    this.dispatch(
+        actionCreators.fetchSuccess(this.getEntityName())(data, Date.now(), this.config.doAppend())
+    );
 };
 
 EntityLifecycle.prototype.dispatchFetchFailure = function (error) {
-  this.dispatch(
-    actionCreators.fetchFailure(this.getEntityName())(error, Date.now())
-  );
+    this.dispatch(
+        actionCreators.fetchFailure(this.getEntityName())(error, Date.now())
+    );
 };
 
-EntityLifecycle.prototype.processStage = function (stage, obj) {
-  if (!stage) throw new Error('Missing required process stage');
-  const processor = this.config.getProcessors()[stage];
-  if (processor) {
-    if (typeof processor !== 'function') throw new Error('processor must be a function');
-    processor(this.dispatch, this.getState, obj);
-  }
+EntityLifecycle.prototype.processStage = function (stage, data) {
+    if (!stage) throw new Error('Missing required process stage');
+    const processor = this.config.getProcessors()[stage];
+    if (processor) {
+        if (typeof processor !== 'function') throw new Error('processor must be a function');
+        return processor(this.dispatch, this.getState, data);
+    }
+    return data;
 };
 
 export default EntityLifecycle;
--- test/unit/test-action-creators.js
+++ test/unit/test-action-creators.js
@@ -2,185 +2,196 @@ import expect from 'expect';
 import thunk from 'redux-thunk';
 import configureStore from 'redux-mock-store';
 
-import { makeActionCreator, makeEntityActionCreator } from '../../src/common/action-creators';
+import {makeActionCreator, makeEntityActionCreator} from '../../src/common/action-creators';
 import actionCreators from '../../src/action-creators';
-import { ACTION_TYPES } from '../../src/common/entity-const';
+import {ACTION_TYPES} from '../../src/common/entity-const';
 
 // Set up mock Redux store
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
 describe('Action Creators', () => {
-  describe('Generic', () => {
-    describe('invoking the makeActionCreator thunk', () => {
-      let type; let key1; let key2; let data1; let
-        data2;
-      beforeEach(() => {
-        type = 'FooType';
-        key1 = 'key1';
-        key2 = 'key2';
-        data1 = 'data1';
-        data2 = 'data2';
-      });
+    describe('Generic', () => {
+        describe('invoking the makeActionCreator thunk', () => {
+            let type;
+            let key1;
+            let key2;
+            let data1;
+            let
+                data2;
+            beforeEach(() => {
+                type = 'FooType';
+                key1 = 'key1';
+                key2 = 'key2';
+                data1 = 'data1';
+                data2 = 'data2';
+            });
 
-      it('should return a function', () => {
-        expect(
-          typeof makeActionCreator(type, key1, key2)
-        ).toBe('function');
-      });
+            it('should return a function', () => {
+                expect(
+                    typeof makeActionCreator(type, key1, key2)
+                ).toBe('function');
+            });
 
-      it('should throw an error if a type is not passed', () => {
-        expect(() => {
-          makeActionCreator();
-        }).toThrow('Type cannot be null/undefined');
-      });
+            it('should throw an error if a type is not passed', () => {
+                expect(() => {
+                    makeActionCreator();
+                }).toThrow('Type cannot be null/undefined');
+            });
 
-      describe('invoking the returned action creator', () => {
-        it('should return an action containing the correct type and keys', () => {
-          const expectedAction = {
-            type,
-            key1: data1,
-            key2: data2
-          };
+            describe('invoking the returned action creator', () => {
+                it('should return an action containing the correct type and keys', () => {
+                    const expectedAction = {
+                        type,
+                        key1: data1,
+                        key2: data2
+                    };
 
-          expect(
-            makeActionCreator(
-              type, key1, key2
-            )(data1, data2)
-          ).toEqual(expectedAction);
+                    expect(
+                        makeActionCreator(
+                            type, key1, key2
+                        )(data1, data2)
+                    ).toEqual(expectedAction);
+                });
+            });
         });
-      });
-    });
-    describe('invoking the makeEntityActionCreator thunk', () => {
-      let type; let entity; let key1; let key2; let data1; let
-        data2;
-      beforeEach(() => {
-        type = 'FooType';
-        entity = 'testModel';
-        key1 = 'key1';
-        key2 = 'key2';
-        data1 = 'data1';
-        data2 = 'data2';
-      });
+        describe('invoking the makeEntityActionCreator thunk', () => {
+            let type;
+            let entity;
+            let key1;
+            let key2;
+            let data1;
+            let
+                data2;
+            beforeEach(() => {
+                type = 'FooType';
+                entity = 'testModel';
+                key1 = 'key1';
+                key2 = 'key2';
+                data1 = 'data1';
+                data2 = 'data2';
+            });
 
-      it('should return a function', () => {
-        expect(
-          typeof makeEntityActionCreator(
-            type, entity, key1, key2
-          )
-        ).toBe('function');
-      });
+            it('should return a function', () => {
+                expect(
+                    typeof makeEntityActionCreator(
+                        type, entity, key1, key2
+                    )
+                ).toBe('function');
+            });
 
-      it('should throw an error if a type is not passed', () => {
-        expect(() => {
-          makeEntityActionCreator();
-        }).toThrow('Type cannot be null/undefined');
-      });
+            it('should throw an error if a type is not passed', () => {
+                expect(() => {
+                    makeEntityActionCreator();
+                }).toThrow('Type cannot be null/undefined');
+            });
 
-      it('should throw an error if an entity is not passed', () => {
-        expect(() => {
-          makeEntityActionCreator(type);
-        }).toThrow('Entity cannot be null/undefined');
-      });
+            it('should throw an error if an entity is not passed', () => {
+                expect(() => {
+                    makeEntityActionCreator(type);
+                }).toThrow('Entity cannot be null/undefined');
+            });
 
-      describe('invoking the returned action creator', () => {
-        it('should return an action containing the correct type and keys', () => {
-          const expectedAction = {
-            type,
-            entity,
-            key1: data1,
-            key2: data2
-          };
+            describe('invoking the returned action creator', () => {
+                it('should return an action containing the correct type and keys', () => {
+                    const expectedAction = {
+                        type,
+                        entity,
+                        key1: data1,
+                        key2: data2
+                    };
 
-          const actionCreator = makeEntityActionCreator(
-            type, entity, key1, key2
-          );
+                    const actionCreator = makeEntityActionCreator(
+                        type, entity, key1, key2
+                    );
 
-          expect(
-            actionCreator(data1, data2)
-          ).toEqual(expectedAction);
+                    expect(
+                        actionCreator(data1, data2)
+                    ).toEqual(expectedAction);
+                });
+            });
         });
-      });
     });
-  });
-  describe('API', () => {
-    let store; let getState; let
-      mockEntity;
-    beforeEach(() => {
-      mockEntity = 'mockEntity';
-      getState = {};
-      store = mockStore(getState);
+    describe('API', () => {
+        let store;
+        let getState;
+        let
+            mockEntity;
+        beforeEach(() => {
+            mockEntity = 'mockEntity';
+            getState = {};
+            store = mockStore(getState);
+        });
+
+        describe('invoking the fetchRequest action creator', () => {
+            it('should create a FETCH_REQUEST action', () => {
+                const fetchRequestAction = {
+                    type: ACTION_TYPES.FETCH_REQUEST,
+                    entity: mockEntity
+                };
+
+                // Under test
+                store.dispatch(
+                    actionCreators.fetchRequest(mockEntity)()
+                );
+
+                expect(
+                    store.getActions()
+                ).toEqual(
+                    [fetchRequestAction]
+                );
+            });
+        });
+
+        describe('invoking the fetchSuccess action creator', () => {
+            const mockData = {foo: 'bar'};
+            const now = Date.now();
+
+            it('should create a FETCH_SUCCESS action', () => {
+                const fetchSuccessAction = {
+                    type: ACTION_TYPES.FETCH_SUCCESS,
+                    entity: mockEntity,
+                    data: mockData,
+                    lastUpdated: now,
+                    append: false
+                };
+
+                // Under test
+                store.dispatch(
+                    actionCreators.fetchSuccess(mockEntity)(mockData, now, false)
+                );
+
+                expect(
+                    store.getActions()
+                ).toEqual(
+                    [fetchSuccessAction]
+                );
+            });
+        });
+
+        describe('invoking the fetchFailure action creator', () => {
+            const mockError = {foo: 'bar'};
+            const now = Date.now();
+
+            it('should create a FETCH_FAILURE action', () => {
+                const fetchFailureAction = {
+                    entity: mockEntity,
+                    type: ACTION_TYPES.FETCH_FAILURE,
+                    error: mockError,
+                    lastUpdated: now
+                };
+
+                // Under test
+                store.dispatch(
+                    actionCreators.fetchFailure(mockEntity)(mockError, now)
+                );
+
+                expect(
+                    store.getActions()
+                ).toEqual(
+                    [fetchFailureAction]
+                );
+            });
+        });
     });
-
-    describe('invoking the fetchRequest action creator', () => {
-      it('should create a FETCH_REQUEST action', () => {
-        const fetchRequestAction = {
-          type: ACTION_TYPES.FETCH_REQUEST,
-          entity: mockEntity
-        };
-
-        // Under test
-        store.dispatch(
-          actionCreators.fetchRequest(mockEntity)()
-        );
-
-        expect(
-          store.getActions()
-        ).toEqual(
-          [fetchRequestAction]
-        );
-      });
-    });
-
-    describe('invoking the fetchSuccess action creator', () => {
-      const mockData = { foo: 'bar' };
-      const now = Date.now();
-
-      it('should create a FETCH_SUCCESS action', () => {
-        const fetchSuccessAction = {
-          type: ACTION_TYPES.FETCH_SUCCESS,
-          entity: mockEntity,
-          data: mockData,
-          lastUpdated: now,
-          append: false
-        };
-
-        // Under test
-        store.dispatch(
-          actionCreators.fetchSuccess(mockEntity)(mockData, now, false)
-        );
-
-        expect(
-          store.getActions()
-        ).toEqual(
-          [fetchSuccessAction]
-        );
-      });
-    });
-
-    describe('invoking the fetchFailure action creator', () => {
-      const mockError = { foo: 'bar' };
-      const now = Date.now();
-
-      it('should create a FETCH_FAILURE action', () => {
-        const fetchFailureAction = {
-          entity: mockEntity,
-          type: ACTION_TYPES.FETCH_FAILURE,
-          error: mockError,
-          lastUpdated: now
-        };
-
-        // Under test
-        store.dispatch(
-          actionCreators.fetchFailure(mockEntity)(mockError, now)
-        );
-
-        expect(
-          store.getActions()
-        ).toEqual(
-          [fetchFailureAction]
-        );
-      });
-    });
-  });
 });
